f27d796a78f6c053a6228732616d38d065ba07f1
tmuxah <tmuxah@gmail.com>
2020-09-25 20:36:38 +0700

    Created simple image viewer
    Added "File" menu and its "Open..." submenu
    Added loading .png and .jpg images
    Added scrollbars for the image when it's larger than the window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +15,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_actionOpen_file_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open"), "", tr("Images (*.png *.jpg)"));
+
+    if (QString::compare(fileName, QString()))
+    {
+        QImage image;
+
+        if (image.load(fileName))
+        {
+            ui->labelImage->setPixmap(QPixmap::fromImage(image));
+        }
+        else
+        {
+            QMessageBox::warning(this, "Warning", "Couldn't load the image.");
+        }
+    }
+}
